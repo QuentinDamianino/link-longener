f3dd26b7a26a4db87c024f873f4fb2441546386b
QuentinDamianino <brdej98@gmail.com>
2021-03-14 04:36:59 +0100

    change onclick to on submit

--- templates/home/home.php
+++ templates/home/home.php
@@ -5,20 +5,22 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <title>Link longener</title>
 </head>
 <body>
 <h1>Hello World</h1>
-<form method="post" id="form">
+<form id="form">
     <label>Link:
-        <input type="text" name="link" id="link" value="1234">
+        <input type="text" name="link" id="link" value="">
     </label>
-    <button type="button" id="submit">Make my link longer</button>
+    <input type="submit" id="submit" value="make my link longer">
 </form>
 <textarea name="newLink" id="newLink" cols="30" rows="10"></textarea>
 <script>
-    const submitButton = document.getElementById("submit");
-    submitButton.addEventListener("click", function () {
+    const form = document.getElementById("form");
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
         const link = document.getElementById('link').value;
         const newLink = document.getElementById("newLink");
 
